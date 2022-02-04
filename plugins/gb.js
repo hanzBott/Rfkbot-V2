abc796f7bfc0bcffbc45075cc8defae78d04b3f8
hanzBott <95454241+hanzBott@users.noreply.github.com>
2022-02-04 21:57:40 +0700

    Update gb.js

--- plugins/gb.js
+++ plugins/gb.js
@@ -1,6 +1,6 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 ye = `@${m.sender.split`@`[0]}`
-let info = `Hai Kak ${ye} Sc? Beli lah Anjeng🗿
+let info = `Sc? Beli lah Anjeng
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', `*${global.packname}*`, 'status@broadcast') 
